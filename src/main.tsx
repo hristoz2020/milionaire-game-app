62e3868f779ca731585c05d4c7bb342f478f88e8
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-17 19:41:07 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<App />
+				</PersistGate>
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+const Home: FC = () => {
+	return (
+		<div className="text-center">
+			<h1>Home Page</h1>
+		</div>
+	);
+};
+
+export default Home;
