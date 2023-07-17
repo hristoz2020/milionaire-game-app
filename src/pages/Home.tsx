4d6cdb0c60d50b588079126c0b29bd64c3cabd49
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-17 20:55:50 +0300

    create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
-
+import { Link } from "react-router-dom";
+import image from "../assets/images/image.webp";
 
 const Home: FC = () => {
 	return (
-		<div className="text-center">
-			<h1>Home Page</h1>
+		<div className="home-page d-flex align-items-center justify-content-center">
+			<div className="d-flex flex-column align-items-center justify-content-center">
+				<img src={image} className="w-25" alt="background-image" />
+				<Link
+					to="/start-game"
+					className="border-2 rounded-5 p-3 text-white bg-dark mt-4 text-decoration-none"
+				>
+					Start Game
+				</Link>
+			</div>
 		</div>
 	);
 };
