87f125951b54c6de13f3a44a0ccdb943e2094e35
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-18 13:18:44 +0300

    added question to game page

---
+++ src/components/QuestionConainer.tsx
@@ -0,0 +1,41 @@
+type Props = {
+	question: string;
+	incorrectOptions: string[];
+	correctOption: string;
+	selectedOption: string | null;
+	onSelectOption: (option: string) => void;
+};
+
+const QuestionConainer: React.FC<Props> = ({
+	question,
+	incorrectOptions,
+	correctOption,
+	selectedOption,
+	onSelectOption,
+}) => {
+	const options: string[] = [...incorrectOptions, correctOption];
+
+	return (
+		<div>
+			<p className="d-flex justify-content-center row bg-white p-4">
+				{question}
+			</p>
+			<div className="row-cols-2 p-1 m-2">
+				{options.map((option, index) => (
+					<button
+						key={index}
+						type="button"
+						className={`bg-light p-3 border-dark rounded ${
+							option === selectedOption ? "blinking-class" : ""
+						}`}
+						onClick={() => onSelectOption(option)}
+					>
+						{option}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default QuestionConainer;
--- src/pages/StartGame.tsx
+++ src/pages/StartGame.tsx
@@ -1,23 +1,54 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import image from "../assets/images/image.webp";
+import QuestionConainer from "../components/QuestionConainer";
 import Timer from "../components/Timer";
 import { getQuestions } from "../redux/slices/questionSlice";
-import { useAppDispatch } from "../redux/store";
+import { RootState, useAppDispatch, useAppSelector } from "../redux/store";
 
 const StartGame: FC = () => {
 	const dispatch = useAppDispatch();
+	const questions = useAppSelector(
+		(state: RootState) => state.questions.question
+	);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+	const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
+	const handleSelectOption = (option: string) => {
+		setSelectedOption(option);
+
+		setTimeout(() => {
+			if (option !== questions[currentQuestionIndex].correct_answer) {
+				window.alert("wrong answer");
+			}
+
+			setSelectedOption(null);
+			setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+		}, 3000);
+	};
+	
 	useEffect(() => {
 		dispatch(getQuestions());
 	}, []);
 
-	
 	return (
 		<div className="game-page">
 			<div className="d-flex flex-column align-items-center justify-content-center mb-5">
-				<img src={image} className="w-25 mt-5 mb-3" alt="background-image" />
+				<img
+					src={image}
+					className="w-25 mt-5 mb-3"
+					alt="background-image"
+				/>
 				<Timer />
 			</div>
+			<QuestionConainer
+				question={questions[currentQuestionIndex].question}
+				incorrectOptions={
+					questions[currentQuestionIndex].incorrect_answers
+				}
+				correctOption={questions[currentQuestionIndex].correct_answer}
+				selectedOption={selectedOption}
+				onSelectOption={handleSelectOption}
+			/>
 		</div>
 	);
 };
