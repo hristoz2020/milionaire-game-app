275b3999ee25218b3e0a811f88239870408106b6
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-21 15:34:35 +0300

    Remove unnecessaty route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/start-game" element={<StartGame />} />
 				<Route path="/score" element={<RewardScale />} />
-				<Route path="*" element={<Home />} />
 			</Routes>
 		</div>
 	);
